de75bc79f491585d1f33ee1fdf01ecfc45b95661
Olivier Traveaux <olivier.traveaux@gmail.com>
2023-09-19 17:39:00 +0200

    3.1 directives

---
+++ src/app/components/directives/directives.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.css'],
+})
+export class DirectivesComponent {
+  title: string = 'Raclette party 🧀';
+  isThisIngredientVital: boolean = false;
+  isGuestListDisplayed: boolean = true;
+  songList: string[] = ['Alejandro', 'Ne me quitte pas', 'Le temps est bon'];
+}
